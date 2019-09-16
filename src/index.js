826a2f7a9004bc3cf166b8124e971233af860f12
JoaquinGomezCabido <joaquingomezcabido@gmail.com>
2019-09-16 16:29:34 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let button = document.querySelector("#submit");
+  let list = document.querySelector("#tasks");
+
+  button.addEventListener("click", function(event) {
+    let content = document.querySelector("#new-task-description").value;
+    createListItem(content);
+    document.querySelector("#new-task-description").value = "";
+    event.preventDefault();
+  });
+
+  function createListItem(task) {
+    let item = document.createElement("li");
+    let text = document.createTextNode(task);
+    let remove = document.createElement("button");
+    remove.textContent = "X";
+    remove.addEventListener("click", function() {
+      this.parentElement.remove();
+    });
+    item.appendChild(text);
+    item.appendChild(remove);
+    list.appendChild(item);
+  }
 });
