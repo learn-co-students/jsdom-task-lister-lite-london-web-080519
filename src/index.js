99a9ee051ddc3e94b15c60f1e84421659804cb73
99chris99 <chrishodge87@gmail.com>
2019-09-16 16:33:54 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+  let button = document.querySelector("#submit");
+
+   button.addEventListener("click", function(event) {
+    let content = document.querySelector("#new-task-description").value;
+    //console.log(content);
+    add_item(content);
+    event.preventDefault();
+    document.querySelector("#new-task-description").value = ""
+   });
+
+  let list = document.querySelector("#tasks");
+  
+  function add_item (content) {
+    var item = document.createElement("li");
+    var text = document.createTextNode(content);
+    var button = document.createElement("button");
+    
+    
+    button.textContent = 'X';
+    button.addEventListener ("click", function () {
+      this.parentElement.remove();
+    });
+    item.appendChild(text);
+    item.appendChild(button);
+    list.appendChild(item);
+    
+  }
+
+
+
+
 });
